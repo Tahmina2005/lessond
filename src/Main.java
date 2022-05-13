143f1853e8c4b6f61b77a7d1791cf423f13beff6
Tahmina2005 <thia4939@gmail.com>
2022-05-13 00:30:36 -0930

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+       
+       public static void main(String[] args){
+          int[] array = new int[15];
+          array[3] = 10;
+          
+              for (int i = 0; i < 15; i++) {
+                     System.out.println(array[3]+" ");
+          }
+
+          double [] d = {-2.5,3.5,2.2,-6.0,4.20,4.4,1.20,45.7,500.6,45.4,66.6,8.2,2.5,5.5,11.3};
+
+
+ //           System.out.println(Arrays.toString(d));
+
+       }
+              
+}
